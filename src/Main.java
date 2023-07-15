5cffc203c6c1b33c0547dad5736745043760123e
Guilherme Henrique <guilherme_h2006@hotmail.com>
2023-07-15 17:52:39 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
-public static void main(String[] args) {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
         String nome = "Guigui";
         String tipoConta = "Corrente";
         double saldo = 2000.00;
@@ -9,3 +12,13 @@ public static void main(String[] args) {
         System.out.println("Tipo conta: " + tipoConta);
         System.out.println("Saldo atual: " + saldo);
         System.out.println("***********************");
+
+        String menu = """
+                ** Digite sua opção **
+                1 - Consultar saldo
+                2 - Transferir valor
+                3 - Receber valor 
+                4 - Sair
+                
+                """;
+        Scanner leitura = new Scanner(System.in);
