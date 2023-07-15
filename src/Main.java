10d8cc6d550c9e213432192831c5fd534d83175a
Guilherme Henrique <guilherme_h2006@hotmail.com>
2023-07-15 17:53:02 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,10 @@ public class Main {
         int opcao = 0;
 
         System.out.println("***********************");
-        System.out.println("Nome do cliente: " + nome);
+        System.out.println("\nNome do cliente: " + nome);
         System.out.println("Tipo conta: " + tipoConta);
         System.out.println("Saldo atual: " + saldo);
-        System.out.println("***********************");
+        System.out.println("\n***********************");
 
         String menu = """
                 ** Digite sua opção **
@@ -22,3 +22,7 @@ public class Main {
                 
                 """;
         Scanner leitura = new Scanner(System.in);
+
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
