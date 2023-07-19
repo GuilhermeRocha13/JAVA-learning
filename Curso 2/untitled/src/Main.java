299bffdcc7cbbf497517078950feeda47bd3d431
Guilherme Henrique <guilherme_h2006@hotmail.com>
2023-07-19 19:54:16 -0300

    Update Main.java

--- Curso 2/untitled/src/Main.java
+++ Curso 2/untitled/src/Main.java
@@ -1,17 +1,51 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String nome = "Guigui";
+        String tipoConta = "Corrente";
+        double saldo = 2000.00;
+        int opcao = 0;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("***********************");
+        System.out.println("\nNome do cliente: " + nome);
+        System.out.println("Tipo conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("\n***********************");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        String menu = """
+                ** Digite sua opção **
+                1 - Consultar saldo
+                2 - Transferir valor
+                3 - Receber valor 
+                4 - Sair
+                
+                """;
+        Scanner leitura = new Scanner(System.in);
+
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+
+            if (opcao == 1){
+                System.out.println("O saldo atualizado é " + saldo);
+            } else if (opcao == 2) {
+                System.out.println("Qual o valor que deseja transferir?");
+                double valor = leitura.nextDouble();
+                if (valor > saldo) {
+                    System.out.println("Não há saldo para realizar a transferência.");
+                } else {
+                    saldo -= valor;
+                    System.out.println("Novo saldo: " + saldo);
+                }
+            } else if (opcao == 3) {
+                System.out.println("Valor recebido: ");
+                double valor = leitura.nextDouble();
+                saldo += valor;
+                System.out.println("Novo saldo: " + saldo);
+            } else if (opcao != 4) {
+                System.out.println("Opção inválida!");
+            }
         }
     }
 }
