66a10a390cc33f8f3fe773d91cb1495ad07c11d3
Guilherme Rocha <guilherme_h2006@hotmail.com>
2023-11-30 11:12:48 -0300

    dividindo branch por projeto

---
+++ Projeto 3/src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -0,0 +1,67 @@
+package br.com.alura.screenmatch.modelos;
+
+
+public class Titulo {
+    private String nome;
+    private int anoLancamento;
+    private boolean incluidoPlano;
+    private double somaAvaliacoes;
+    private int totalAvaliacao;
+    private int duracaoMinutos;
+
+    //Getters e setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+
+
+    public double getSomaAvaliacoes() {
+        return somaAvaliacoes;
+    }
+
+    public int getTotalAvaliacao() {
+        return totalAvaliacao;
+    }
+
+    public int getDuracaoMinutos() {
+        return duracaoMinutos;
+    }
+
+    public void setDuracaoMinutos(int duracaoMinutos) {
+        this.duracaoMinutos = duracaoMinutos;
+    }
+
+    //metodos da classe
+    public void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " + getNome());
+        System.out.println("Ano de lançamento: " + getAnoLancamento());
+        System.out.println("Duração em minutos " + getDuracaoMinutos());
+    }
+
+    public void exibeAvaliacoes(){
+        System.out.println("Total de avaliações: " + getTotalAvaliacao());
+        System.out.println("Soma das avaliações: " + getSomaAvaliacoes());
+    }
+
+    public void avaliacao(double nota){
+        somaAvaliacoes += nota;
+        totalAvaliacao ++;
+    }
+
+    public double pegaMedia(){
+        return somaAvaliacoes / totalAvaliacao;
+    }
+}
